ddf6073fd39c0954f6a28cf4deb6b30b2f4df309
Pankaj Kumar <pankajkumar123014@gmail.com>
2023-04-05 18:26:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -11,22 +11,9 @@ const lookup = {
 };
 
 function rot13(str) {
-    var codeArr = [], seq, final = "";
-    for (var i = 0; i < str.length; i++) {
-        uCode = str.charCodeAt([i]);
-        if ((uCode > 64 && uCode < 78) || (uCode > 96 && uCode < 110)) {
-            uCode += 13;
-            codeArr.push(uCode);
-        } else if ((uCode > 77 && uCode < 91) || (uCode > 109 && uCode < 123)){
-            uCode -= 13;
-            codeArr.push(uCode);
-        } else { codeArr.push(uCode); }
-    }
-    console.log(codeArr);
-    for (var j = 0; j < codeArr.length; j++) {
-        final += String.fromCharCode(codeArr[j]);
-    }
-    return final;
+    const originalAlpha = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ;
+  const cipher = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM;
+  return str.replace(/[a-z]/gi, letter => cipher[originalAlpha.indexOf(letter)]);
 }
 
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
